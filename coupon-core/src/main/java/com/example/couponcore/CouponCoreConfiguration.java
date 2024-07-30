6be12b85305a342adb4e3a19f6fb918f5950e787
leevelop <merong0075@gmail.com>
2024-07-31 00:36:40 +0900

    폴더 업로드

---
+++ coupon-core/src/main/java/com/example/couponcore/CouponCoreConfiguration.java
@@ -0,0 +1,9 @@
+package com.example.couponcore;
+
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.stereotype.Component;
+
+@Component
+@EnableAutoConfiguration
+public class CouponCoreConfiguration {
+}
