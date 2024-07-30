4abf11fa40c5054ae60f847c90842baf4228fa97
leevelop <merong0075@gmail.com>
2024-07-30 21:45:40 +0900

    작업 환경 구축

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,8 @@
+val bootJar: org.springframework.boot.gradle.tasks.bundling.BootJar by tasks
+
+bootJar.enabled = false
+
+
 plugins {
     java
     id("org.springframework.boot") version "3.3.2"
@@ -17,12 +22,33 @@ repositories {
     mavenCentral()
 }
 
-dependencies {
-    implementation("org.springframework.boot:spring-boot-starter")
-    testImplementation("org.springframework.boot:spring-boot-starter-test")
-    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
-}
+subprojects {
+    apply(plugin = "java")
+    apply(plugin = "io.spring.dependency-management")
+    apply(plugin = "org.springframework.boot")
 
+    repositories {
+        mavenCentral()
+    }
+
+    dependencies {
+        implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+        implementation("org.springframework.boot:spring-boot-starter-data-redis")
+        compileOnly("org.projectlombok:lombok")
+        annotationProcessor("org.projectlombok:lombok")
+        runtimeOnly("com.h2database:h2")
+        runtimeOnly("com.mysql:mysql-connector-j")
+        runtimeOnly("org.mariadb.jdbc:mariadb-java-client:2.7.3")
+        implementation("org.springframework.boot:spring-boot-starter")
+        implementation("com.querydsl:querydsl-jpa:5.0.0:jakarta")
+        implementation("org.springframework.boot:spring-boot-starter-actuator")
+        implementation("io.micrometer:micrometer-registry-prometheus")
+        annotationProcessor("com.querydsl:querydsl-apt:5.0.0:jakarta")
+        annotationProcessor("jakarta.annotation:jakarta.annotation-api")
+        annotationProcessor("jakarta.persistence:jakarta.persistence-api")
+        testImplementation("org.springframework.boot:spring-boot-starter-test")
+    }
+}
 tasks.withType<Test> {
     useJUnitPlatform()
 }
