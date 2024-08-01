810c3170f06ea7add12cecc621934de62548b19c
leevelop <merong0075@gmail.com>
2024-08-02 00:35:34 +0900

    부하테스트 진행

--- coupon-api/src/main/java/com/example/couponapi/controller/HelloController.java
+++ coupon-api/src/main/java/com/example/couponapi/controller/HelloController.java
@@ -7,7 +7,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 public class HelloController {
     @GetMapping("/hello")
-    public String hello() {
+    public String hello() throws InterruptedException {
+        Thread.sleep(500); //400 rps 정도 나온다.
         return "Hello, World!";
-    }
+    }// 초당 2건을 처리 *N(서버에서 동시에 처리할 수 있느 수(톱켓에서 스레드풀의 맥스는 200으로 잡혀있음)) = 400 정도 나옴
 }
